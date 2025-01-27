d607ca68ca20bee05a9c0020b6b8c4f06529942d
maria <maria.puerta.sala@gmail.com>
2025-01-27 15:58:52 -0500

    cambios realizados

---
+++ data/story.js
@@ -0,0 +1,614 @@
+var story = {
+ "docName": "Untitled",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "jpg",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Untitled",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "2:20",
+   "groupIndex": 0,
+   "title": "Bienvenida",
+   "image": "bienvenida.jpg",
+   "index": 0,
+   "width": 446,
+   "height": 859,
+   "x": -539,
+   "y": 34,
+   "awidth": 446,
+   "aheight": 859,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Bienvenida",
+     "rect": {
+      "x": 0,
+      "y": 0,
+      "width": 446,
+      "height": 859
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 3,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "7:96",
+   "groupIndex": 0,
+   "title": "Uñas",
+   "image": "uñas.jpg",
+   "index": 1,
+   "width": 444,
+   "height": 956,
+   "x": -491,
+   "y": 3071,
+   "awidth": 444,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 2",
+     "rect": {
+      "x": 16,
+      "y": 519,
+      "width": 358,
+      "height": 73
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 4,
+       "disableAutoScroll": true,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "2:22",
+   "groupIndex": 0,
+   "title": "Iniciar sesion",
+   "image": "iniciar-sesion.jpg",
+   "index": 2,
+   "width": 409,
+   "height": 956,
+   "x": -473,
+   "y": 1992,
+   "awidth": 409,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "INICIO",
+     "rect": {
+      "x": 21,
+      "y": 34,
+      "width": 111,
+      "height": 37
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "INICIO",
+     "rect": {
+      "x": 21,
+      "y": 34,
+      "width": 111,
+      "height": 37
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Rectangle 3",
+     "rect": {
+      "x": 118,
+      "y": 758,
+      "width": 172,
+      "height": 42
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "2:32",
+   "groupIndex": 0,
+   "title": "inicio",
+   "image": "inicio.jpg",
+   "index": 3,
+   "width": 440,
+   "height": 956,
+   "x": -17,
+   "y": 46,
+   "awidth": 440,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "menu 1",
+     "rect": {
+      "x": 41,
+      "y": 69,
+      "width": 30,
+      "height": 30
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "No tienes cuenta con nosotros! registrate aqui",
+     "rect": {
+      "x": 76,
+      "y": 604,
+      "width": 262,
+      "height": 43
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 8,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "usuario 1",
+     "rect": {
+      "x": 343,
+      "y": 69,
+      "width": 30,
+      "height": 30
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "15:181",
+   "groupIndex": 0,
+   "title": "Uñas(1)",
+   "image": "uñas(1).jpg",
+   "index": 4,
+   "width": 438,
+   "height": 956,
+   "x": 218,
+   "y": 3071,
+   "awidth": 438,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null
+  },
+  {
+   "id": "23:184",
+   "groupIndex": 0,
+   "title": "Servicios",
+   "image": "servicios.jpg",
+   "index": 5,
+   "width": 441,
+   "height": 956,
+   "x": 268,
+   "y": 1992,
+   "awidth": 441,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 2",
+     "rect": {
+      "x": 208,
+      "y": 273,
+      "width": 169,
+      "height": 145
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 1,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "23:49",
+   "groupIndex": 0,
+   "title": "Nosotros",
+   "image": "nosotros.jpg",
+   "index": 6,
+   "width": 446,
+   "height": 1654,
+   "x": 529,
+   "y": 10,
+   "awidth": 446,
+   "aheight": 1654,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Servicios",
+     "rect": {
+      "x": 78,
+      "y": 81,
+      "width": 119,
+      "height": 27
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 7,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "No tienes cuenta con nosotros! registrate aqui",
+     "rect": {
+      "x": 108,
+      "y": 618,
+      "width": 262,
+      "height": 43
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 8,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "usuario 1",
+     "rect": {
+      "x": 366,
+      "y": 65,
+      "width": 30,
+      "height": 30
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 2,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "23:2",
+   "groupIndex": 0,
+   "title": "Servicios(1)",
+   "image": "servicios(1).jpg",
+   "index": 7,
+   "width": 441,
+   "height": 956,
+   "x": 1149,
+   "y": 34,
+   "awidth": 441,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 2",
+     "rect": {
+      "x": 205,
+      "y": 264,
+      "width": 169,
+      "height": 145
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 1,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Registrate!",
+     "rect": {
+      "x": 153,
+      "y": 799,
+      "width": 114,
+      "height": 13
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 8,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "4:67",
+   "groupIndex": 0,
+   "title": "Registrase",
+   "image": "registrase.jpg",
+   "index": 8,
+   "width": 439,
+   "height": 956,
+   "x": 1751,
+   "y": 34,
+   "awidth": 439,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "INICIO",
+     "rect": {
+      "x": -17,
+      "y": 14,
+      "width": 111,
+      "height": 37
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 8,
+       "frameIndex": 3,
+       "transAnimType": 3,
+       "transAnimDuration": 0.30000001192092896,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Registrarse",
+     "rect": {
+      "x": 142,
+      "y": 705,
+      "width": 114,
+      "height": 13
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 8,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "9:112",
+   "groupIndex": 0,
+   "title": "Servicios(2)",
+   "image": "servicios(2).jpg",
+   "index": 9,
+   "width": 409,
+   "height": 956,
+   "x": 2277,
+   "y": 34,
+   "awidth": 409,
+   "aheight": 956,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 10,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#FFFFFF"
+  }
+ ]
+}
